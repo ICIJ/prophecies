cd7c5abf60ce8771e1e5f4257834bc0736931cdc
Pierre Romera <hello@pirhoo.com>
2021-07-25 20:40:14 +0000

    feat: change env prefix for Setting

--- prophecies/apps/api/models/setting.py
+++ prophecies/apps/api/models/setting.py
@@ -6,17 +6,19 @@ import os
 
 
 class SettingManager(models.Manager):
+    env_prefix = 'PROPHECIES_APP_'
+
     def env(self):
         items = os.environ.items()
-        # Create a dict with only env variables that starts with 'APP_'
-        return dict(item for item in items if item[0].startswith('APP_'))
+        # Create a dict with only env variables that starts with 'PROPHECIES_'
+        return dict(item for item in items if item[0].startswith(self.env_prefix))
 
     def from_env(self):
         settings = []
         # A dict of static settings targeting to be visible publicaly
         for (key, value) in Setting.objects.env().items():
             # Remove the 'APP_' part from the key and convert to camel case
-            key = to_camel_case(key[4:])
+            key = to_camel_case(key[len(self.env_prefix):])
             # Finally, add the setting to the list
             settings.append(Setting(key=key, value=value))
         return settings
--- prophecies/apps/api/tests/test_setting.py
+++ prophecies/apps/api/tests/test_setting.py
@@ -15,13 +15,13 @@ class TestSetting(TestCase):
         self.assertIn({ 'key': 'defaultLocale', 'value': 'en' }, request.json())
 
 
-    @mock.patch.dict(os.environ, {"APP_FOO": "bar"})
+    @mock.patch.dict(os.environ, {"PROPHECIES_APP_FOO": "bar"})
     def test_it_returns_setting_from_env(self):
         self.client.login(username='olivia', password='olivia')
         request = self.client.get('/api/v1/settings.json')
         self.assertIn({ 'key': 'foo', 'value': 'bar' }, request.json())
 
-    @mock.patch.dict(os.environ, {"APP_FOO_BAZ": "bar"})
+    @mock.patch.dict(os.environ, {"PROPHECIES_APP_FOO_BAZ": "bar"})
     def test_it_returns_camel_case_setting_from_env(self):
         self.client.login(username='olivia', password='olivia')
         request = self.client.get('/api/v1/settings.json')
