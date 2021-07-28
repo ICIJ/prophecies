e4e1550789223090323209994ea2f034ff53d0b9
Pierre Romera <hello@pirhoo.com>
2021-07-28 07:47:51 +0000

    feat: add TaskStatsCard

---
+++ prophecies/apps/frontend/src/components/TaskStatsCard.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="card card-body shadow d-flex">
+    <div></div>
+    <div>
+      <div class="d-flex align-items-center mb-3">
+        <h3 class="m-0">
+          {{ task.name }}
+          <b-badge class="bg-transparent font-weight-normal text-muted">
+            {{ task.project.name }}
+          </b-badge>
+        </h3>
+        <span class="bg-warning rounded py-1 px-2 ml-auto">
+          Priority {{ task.priority }}
+        </span>
+      </div>
+      <p>
+        Fully checked items:
+        <span class="text-danger font-weight-bold ml-2">
+          0 / 55
+        </span>
+      </p>
+      <div class="d-flex align-items-center">
+        <ul class="list-inline m-0">
+          <li class="list-inline-item">
+            Round 1
+            <span class="font-weight-bold ml-3">
+              77%
+            </span>
+            <span class="text-muted mx-2">
+              |
+            </span>
+          </li>
+          <li class="list-inline-item">
+            Round 2
+            <span class="font-weight-bold ml-3">
+              10%
+            </span>
+            <span class="text-muted mx-2">
+              |
+            </span>
+          </li>
+          <li class="list-inline-item">
+            Round 3
+            <span class="font-weight-bold ml-3">
+              0%
+            </span>
+          </li>
+        </ul>
+        <span class="bg-primary text-white font-weight-bold rounded py-1 px-2 ml-auto">
+          29%
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Task from '@/models/Task'
+
+export default {
+  name: 'TaskStatsCard',
+  props: {
+    task: {
+      type: Object
+    }
+  }
+}
+</script>
