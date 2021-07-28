8bb5d4b65eb3c3df8f9ce6dc3cf0c5dc93a808c9
Pierre Romera <hello@pirhoo.com>
2021-07-28 07:47:23 +0000

    feat: add component for app-wide spinner

---
+++ prophecies/apps/frontend/src/components/AppWaiter.vue
@@ -0,0 +1,40 @@
+<script>
+/**
+ * A component that wraps the v-wait component to provide a unique spinner
+ * experience between views and components.
+ */
+export default {
+  name: 'AppWaiter',
+  props: {
+    duration: {
+      type: Number,
+      default: 400
+    },
+    mode: {
+      type: String,
+      default: 'out-in'
+    },
+    loader: {
+      type: String
+    },
+    small: {
+      type: Boolean
+    },
+    transition: {
+      type: String,
+      default: 'fade'
+    },
+    waiterClass: {
+      type: [String, Array, Object],
+      default: () => (['mx-auto', 'my-1', 'd-block'])
+    }
+  }
+}
+</script>
+
+<template>
+  <v-wait :for="loader" :transition="transition" :mode="mode" :duration="duration">
+    <b-spinner small variant="light" slot="waiting" :class="waiterClass" />
+    <slot />
+  </v-wait>
+</template>
