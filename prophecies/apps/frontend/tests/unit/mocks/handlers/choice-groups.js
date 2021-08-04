847cd0475a5445c45d309ed80fc7b63226bea656
Pierre Romera <hello@pirhoo.com>
2021-08-04 18:59:48 +0200

    test: add MSW handler for ChoiceGroup

---
+++ prophecies/apps/frontend/tests/unit/mocks/handlers/choice-groups.js
@@ -0,0 +1,268 @@
+import { rest } from 'msw'
+
+export default [
+  rest.get('/api/v1/task-record-reviews', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        links: {
+          first: 'http://localhost/api/v1/choice-groups/?page%5Bnumber%5D=1',
+          last: 'http://localhost/api/v1/choice-groups/?page%5Bnumber%5D=1',
+          next: null,
+          prev: null
+        },
+        data: [
+          {
+            type: 'ChoiceGroup',
+            id: '2',
+            attributes: {
+              name: 'Which pastry?'
+            },
+            relationships: {
+              choices: {
+                meta: {
+                  count: 3
+                },
+                data: [
+                  {
+                    type: 'Choice',
+                    id: '4'
+                  },
+                  {
+                    type: 'Choice',
+                    id: '5'
+                  },
+                  {
+                    type: 'Choice',
+                    id: '6'
+                  }
+                ]
+              }
+            },
+            links: {
+              self: 'http://localhost/api/v1/choice-groups/2/'
+            }
+          },
+          {
+            type: 'ChoiceGroup',
+            id: '1',
+            attributes: {
+              name: 'Is it correct?'
+            },
+            relationships: {
+              choices: {
+                meta: {
+                  count: 3
+                },
+                data: [
+                  {
+                    type: 'Choice',
+                    id: '1'
+                  },
+                  {
+                    type: 'Choice',
+                    id: '2'
+                  },
+                  {
+                    type: 'Choice',
+                    id: '3'
+                  }
+                ]
+              }
+            },
+            links: {
+              self: 'http://localhost/api/v1/choice-groups/1/'
+            }
+          }
+        ],
+        included: [
+          {
+            type: 'Choice',
+            id: '1',
+            attributes: {
+              name: 'Correct',
+              value: 'correct'
+            }
+          },
+          {
+            type: 'Choice',
+            id: '2',
+            attributes: {
+              name: 'Incorrect',
+              value: 'incorrect'
+            }
+          },
+          {
+            type: 'Choice',
+            id: '3',
+            attributes: {
+              name: 'Unkown',
+              value: 'unkown'
+            }
+          },
+          {
+            type: 'Choice',
+            id: '4',
+            attributes: {
+              name: 'Babka',
+              value: 'Babka'
+            }
+          },
+          {
+            type: 'Choice',
+            id: '5',
+            attributes: {
+              name: 'Croissant',
+              value: 'Croissant'
+            }
+          },
+          {
+            type: 'Choice',
+            id: '6',
+            attributes: {
+              name: 'Muffin',
+              value: 'Muffin'
+            }
+          }
+        ],
+        meta: {
+          pagination: {
+            page: 1,
+            pages: 1,
+            count: 2
+          }
+        }
+      })
+    )
+  }),
+
+  rest.get('/api/v1/task-record-reviews/1', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        data: {
+          type: 'ChoiceGroup',
+          id: '1',
+          attributes: {
+            name: 'Is it correct?'
+          },
+          relationships: {
+            choices: {
+              meta: {
+                count: 3
+              },
+              data: [
+                {
+                  type: 'Choice',
+                  id: '1'
+                },
+                {
+                  type: 'Choice',
+                  id: '2'
+                },
+                {
+                  type: 'Choice',
+                  id: '3'
+                }
+              ]
+            }
+          },
+          links: {
+            self: 'http://0.0.0.0:8008/api/v1/choice-groups/1/'
+          }
+        },
+        included: [
+          {
+            type: 'Choice',
+            id: '1',
+            attributes: {
+              name: 'Correct',
+              value: 'correct'
+            }
+          },
+          {
+            type: 'Choice',
+            id: '2',
+            attributes: {
+              name: 'Incorrect',
+              value: 'incorrect'
+            }
+          },
+          {
+            type: 'Choice',
+            id: '3',
+            attributes: {
+              name: 'Unkown',
+              value: 'unkown'
+            }
+          }
+        ]
+      })
+    )
+  }),
+
+  rest.get('/api/v1/task-record-reviews/2', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        data: {
+          type: 'ChoiceGroup',
+          id: '2',
+          attributes: {
+            name: 'Which pastry?'
+          },
+          relationships: {
+            choices: {
+              meta: {
+                count: 3
+              },
+              data: [
+                {
+                  type: 'Choice',
+                  id: '4'
+                },
+                {
+                  type: 'Choice',
+                  id: '5'
+                },
+                {
+                  type: 'Choice',
+                  id: '6'
+                }
+              ]
+            }
+          },
+          links: {
+            self: 'http://0.0.0.0:8008/api/v1/choice-groups/2/'
+          }
+        },
+        included: [
+          {
+            type: 'Choice',
+            id: '4',
+            attributes: {
+              name: 'Babka',
+              value: 'Babka'
+            }
+          },
+          {
+            type: 'Choice',
+            id: '5',
+            attributes: {
+              name: 'Croissant',
+              value: 'Croissant'
+            }
+          },
+          {
+            type: 'Choice',
+            id: '6',
+            attributes: {
+              name: 'Muffin',
+              value: 'Muffin'
+            }
+          }
+        ]
+      })
+    )
+  })
+]
