8fbeca60aed0c514f26845eaee243a9562467e55
Pierre Romera <hello@pirhoo.com>
2021-08-17 17:50:26 +0200

    feat: add LOGGING to production

--- prophecies/settings/base.py
+++ prophecies/settings/base.py
@@ -1,6 +1,7 @@
-from os.path import normpath, exists
 import sys
 import environ
+
+from os.path import normpath, exists
 
 root = environ.Path(__file__) - 3
 project_root = root.path('prophecies')
--- prophecies/settings/production.py
+++ prophecies/settings/production.py
@@ -1,8 +1,36 @@
+import environ
 from .base import *
+from os.path import exists
 
-DEBUG = False
-TEMPLATE_DEBUG = False
+root = environ.Path(__file__) - 3
+# Read .env from the root path and load environment variables
+env = environ.Env()
+env_root = root.path('.env').root
+# Load the .env file only if it exists
+if exists(env_root):
+    env.read_env(env_root)
+
+
+DEBUG = env.bool('DEBUG', default=False)
+TEMPLATE_DEBUG = env.bool('DEBUG', default=False)
 USE_X_FORWARDED_HOST = False
 
 INSTALLED_APPS.remove('debug_toolbar')
 MIDDLEWARE.remove('debug_toolbar.middleware.DebugToolbarMiddleware')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': env.str('DJANGO_LOG_LEVEL', 'INFO'),
+            'propagate': True,
+        },
+    },
+}
