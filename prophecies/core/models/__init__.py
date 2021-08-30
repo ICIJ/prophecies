c5873a74bb9731da210aa36fc4853163ab295f70
madeline oleary <madeline.w.oleary@gmail.com>
2021-08-30 13:48:49 +0000

    feat: adds the tip model

--- prophecies/core/models/__init__.py
+++ prophecies/core/models/__init__.py
@@ -7,3 +7,4 @@ from .task import Task
 from .task_checker import TaskChecker
 from .task_record import TaskRecord
 from .task_record_review import TaskRecordReview
+from .tip import Tip
---
+++ prophecies/core/models/tip.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+from prophecies.core.models import Project, Task
+
+class Tip(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Tip name", null=True, blank=True)
+    creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    task = models.ForeignKey(Task, on_delete=models.SET_NULL, null=True, blank=True)
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
