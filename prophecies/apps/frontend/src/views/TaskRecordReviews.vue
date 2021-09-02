fc462486d788dd5e0d2595b6452a6e0cb5de3ca9
madeline oleary <madeline.w.oleary@gmail.com>
2021-09-02 13:07:51 +0000

    feat: fixing grammar in title

--- prophecies/apps/frontend/src/views/TaskRecordReviews.vue
+++ prophecies/apps/frontend/src/views/TaskRecordReviews.vue
@@ -275,7 +275,7 @@ export default {
         await this.waitFor(this.fetchAllLoader, this.fetchAll)
         this.$core.setPageTitle(this.task.name)
       } catch (error) {
-        const title = 'Unable to found this task'
+        const title = 'Unable to find this task'
         this.$router.replace({ name: 'error', params: { title, error } })
       }
     },
