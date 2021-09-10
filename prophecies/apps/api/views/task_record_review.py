2fdba2f77a887ef2dc8fed24acbbf9a4461a9e38
madeline oleary <madeline.w.oleary@gmail.com>
2021-09-10 13:16:38 +0000

    fix: removes pdb trace

--- prophecies/apps/api/views/task_record_review.py
+++ prophecies/apps/api/views/task_record_review.py
@@ -134,8 +134,6 @@ class TaskRecordReviewViewSet(viewsets.ModelViewSet):
             .select_related('task_record__task__project')
 
     def check_object_permissions(self, request, obj):
-        if obj.task_record.locked:
-            import pdb; pdb.set_trace()
         if obj.checker != request.user:
             raise exceptions.PermissionDenied(detail='You do not have permission to update this resource.')
         if obj.task_record.locked and not [*request.data] == ['id', 'note']:
