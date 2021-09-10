5f360e518278993153e1179dd9dcbc5716e9626e
madeline oleary <madeline.w.oleary@gmail.com>
2021-09-10 14:48:05 +0000

    fix: checks more explicitly for choice in request payload to trigger 403

--- prophecies/apps/api/views/task_record_review.py
+++ prophecies/apps/api/views/task_record_review.py
@@ -136,5 +136,5 @@ class TaskRecordReviewViewSet(viewsets.ModelViewSet):
     def check_object_permissions(self, request, obj):
         if obj.checker != request.user:
             raise exceptions.PermissionDenied(detail='You do not have permission to update this resource.')
-        if obj.task_record.locked and not [*request.data] == ['id', 'note']:
+        if 'choice' in request.data and obj.task_record.locked:
             raise exceptions.PermissionDenied(detail='This resource was locked by a user.')
