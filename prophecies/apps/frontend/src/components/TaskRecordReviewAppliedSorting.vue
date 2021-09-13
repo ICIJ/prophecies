c85ded7bd5295333dee75180d9a464965c8d11b4
madeline oleary <madeline.w.oleary@gmail.com>
2021-09-13 11:40:48 +0000

    fix: removes styling from applied sort to conform with original figma design

--- prophecies/apps/frontend/src/components/TaskRecordReviewAppliedSorting.vue
+++ prophecies/apps/frontend/src/components/TaskRecordReviewAppliedSorting.vue
@@ -13,12 +13,12 @@
       sortOptions () {
         return [
           { value: 'task_record__id', label: 'ID (default)', $isDefault: true },
-          { value: 'task_record__predicted_value', label: 'Predicted value (A - Z)' },
-          { value: '-task_record__predicted_value', label: 'Predicted value (Z - A)' },
-          { value: 'task_record__original_value', label: 'Original value (A - Z)' },
-          { value: '-task_record__original_value', label: 'Original value (Z - A)' },
-          { value: 'task_record__priority', label: 'Priority (low to high)', $isDisabled: true },
-          { value: '-task_record__priority', label: 'Priority (high to low)', $isDisabled: true },
+          { value: 'task_record__predicted_value', label: 'Sorted by: predicted value (A - Z)' },
+          { value: '-task_record__predicted_value', label: 'Sorted by: predicted value (Z - A)' },
+          { value: 'task_record__original_value', label: 'Sorted by: original value (A - Z)' },
+          { value: '-task_record__original_value', label: 'Sorted by: original value (Z - A)' },
+          { value: 'task_record__priority', label: 'Sorted by: priority (low to high)', $isDisabled: true },
+          { value: '-task_record__priority', label: 'Sorted by: priority (high to low)', $isDisabled: true },
         ]
       },
       selectedSortOption () {
@@ -41,7 +41,7 @@
 
 <template lang="html">
   <div class="task-record-review-applied-sort mb-4">
-    <b-btn variant="primary" class="task-record-review-applied-sort__item" :title="selectedSortOption['label']"  v-b-tooltip.hover @click="deleteSortOption(selectedSortOption['value'])" v-if="hasSorting">
+    <b-btn variant="default" class="task-record-review-applied-sort__item" :title="selectedSortOption['label']"  v-b-tooltip.hover @click="deleteSortOption(selectedSortOption['value'])" v-if="hasSorting">
       {{ selectedSortOption['label'] }} <x-icon class="ml-3" size="1x" />
     </b-btn>
   </div>
@@ -51,15 +51,10 @@
   .task-record-review-applied-sort {
     &__item {
       font-weight: bold;
+      text-decoration: underline;
       padding-left: $spacer;
       padding-right: $spacer;
       margin-right: $spacer;
-    }
-
-    & /deep/ &__item.btn:hover {
-      background-color: $warning;
-      border-color: $warning;
-      color: text-contrast($warning);
     }
   }
 </style>
