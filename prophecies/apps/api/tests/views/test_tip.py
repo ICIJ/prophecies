3de3cabe503f7f184af2be7161ae3e12911267a3
madeline oleary <madeline.w.oleary@gmail.com>
2021-09-23 09:47:57 +0000

    test: tests that tips are scoped to the authenticated user

--- prophecies/apps/api/tests/views/test_tip.py
+++ prophecies/apps/api/tests/views/test_tip.py
@@ -1,25 +1,37 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
-from prophecies.core.models import Project, Task, Tip
+from django.contrib.auth.models import User
+from prophecies.core.models import Project, Task, Tip, TaskChecker
 
 class TestTip(TestCase):
     client = APIClient()
     fixtures = ['users.json']
 
     def setUp(self):
+        self.olivia = User.objects.get(username='olivia')
         self.project_foo = Project.objects.create(name='foo')
         self.task_paintings = Task.objects.create(name='paintings', project=self.project_foo)
         self.tip_bar = Tip.objects.create(name='bar', description='@user try this', task=self.task_paintings, project=self.project_foo)
         self.tip_top = Tip.objects.create(name='top', description='@user try this instead', task=self.task_paintings, project=self.project_foo)
+        TaskChecker.objects.create(task=self.task_paintings, checker=self.olivia)
 
     def test_it_returns_all_tips(self):
-        self.client.login(username='olivia', password='olivia')
+        self.client.login(username=self.olivia, password='olivia')
+        request = self.client.get('/api/v1/tips/')
+        data = request.json().get('data')
+        self.assertEqual(len(data), 2)
+
+    def test_it_does_not_return_tips_from_which_the_user_is_restricted(self):
+        self.client.login(username=self.olivia, password='olivia')
+        project_bar = Project.objects.create(name='bar')
+        task_cars = Task.objects.create(name='cars', project=project_bar)
+        Tip.objects.create(name='baz', description='@user try this', task=task_cars, project=project_bar)
         request = self.client.get('/api/v1/tips/')
         data = request.json().get('data')
         self.assertEqual(len(data), 2)
 
     def test_details_returns_bar_tip(self):
-        self.client.login(username='olivia', password='olivia')
+        self.client.login(username=self.olivia, password='olivia')
         id = self.tip_bar.id
         request = self.client.get('/api/v1/tips/%s/' % id)
         self.assertEqual(request.status_code, 200)
@@ -27,7 +39,7 @@ class TestTip(TestCase):
         self.assertEqual(data['attributes']['name'], 'bar')
 
     def test_details_returns_top_tip(self):
-        self.client.login(username='olivia', password='olivia')
+        self.client.login(username=self.olivia, password='olivia')
         id = self.tip_top.id
         request = self.client.get('/api/v1/tips/%s/' % id)
         self.assertEqual(request.status_code, 200)
