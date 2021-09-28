b2c2ef4a877f200414087f7aff0a801fa11cd60e
madeline oleary <madeline.w.oleary@gmail.com>
2021-09-28 14:00:20 +0000

    fix #66 : displays project and task mentions in danger styling

--- prophecies/apps/frontend/src/models/Tip.js
+++ prophecies/apps/frontend/src/models/Tip.js
@@ -38,10 +38,7 @@ export default class Tip extends Model {
 
   get descriptionWithMentions () {
     return String(this.description).replace(User.usernamePattern, (match, p1) => {
-      if (User.me() && p1 === User.me().username) {
-        return `<span class="mention mention--is-me">${match}</span>`
-      }
-      return `<span class="mention">${match}</span>`
+      return `<span class="mention mention--is-me">${match}</span>`
     })
   }
 
