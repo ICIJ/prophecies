1aee1db1b6cefd5fdb78935c644046316e0c9821
Pierre Romera <hello@pirhoo.com>
2021-10-14 09:40:22 +0000

    refactor: use base serializer for settings

--- prophecies/apps/api/views/setting.py
+++ prophecies/apps/api/views/setting.py
@@ -1,32 +1,24 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, serializers
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
-from rest_framework_json_api import serializers
 from prophecies.core.models.setting import Setting
 
 
-class SettingSerializer(serializers.ModelSerializer):
+class SettingSerializer(serializers.Serializer):
     key = serializers.CharField()
     value = serializers.CharField()
     public = serializers.BooleanField()
-    search_fields = ['key', 'value']
-    ordering_fields = ['key']
-    ordering = ['key']
-    filterset_fields = ['key', 'value']
-
-    class Meta:
-        model = Setting
-        fields = ['key', 'value', 'public']
 
 
-class SettingViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Setting.objects.none()
+class SettingViewSet(viewsets.ViewSet):
     serializer_class = SettingSerializer
     pagination_class = None
+    resource_name = 'Setting'
+    http_method_names = ['get', 'head']
     permission_classes = []
 
 
-    def get_queryset(self):        
+    def get_queryset(self):                
         if not self.request.user.is_authenticated:
             return Setting.objects.public()
         else:
@@ -35,7 +27,7 @@ class SettingViewSet(viewsets.ReadOnlyModelViewSet):
 
     def list(self, request, **kwargs):
         settings = self.get_queryset()
-        serializer = SettingSerializer(settings, context={'request': request}, many=True)
+        serializer = SettingSerializer(settings, many=True, context={'request': request})
         return Response(serializer.data)
 
 
