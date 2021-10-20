101587ffff478c3c5a6917ce10e1e3a7ed24dd98
Caroline Desprat <cdesprat@icij.org>
2021-10-20 09:47:22 +0000

    feat: add tutorial display to task record review single view

--- prophecies/apps/frontend/src/views/TaskRecordReviewRetrieve.vue
+++ prophecies/apps/frontend/src/views/TaskRecordReviewRetrieve.vue
@@ -1,108 +1,111 @@
 <script>
-  import { find, get, uniqueId } from 'lodash'
+import { find, get, uniqueId } from 'lodash'
 
-  import AppBreadcrumb from '@/components/AppBreadcrumb'
-  import AppHeader from '@/components/AppHeader'
-  import AppWaiter from '@/components/AppWaiter'
-  import TaskRecordReviewCard from '@/components/TaskRecordReviewCard'
-  import ChoiceGroup from '@/models/ChoiceGroup'
-  import Task from '@/models/Task'
-  import TaskRecordReview from '@/models/TaskRecordReview'
+import AppBreadcrumb from '@/components/AppBreadcrumb'
+import AppHeader from '@/components/AppHeader'
+import AppWaiter from '@/components/AppWaiter'
+import TaskRecordReviewCard from '@/components/TaskRecordReviewCard'
+import ChoiceGroup from '@/models/ChoiceGroup'
+import Task from '@/models/Task'
+import TaskRecordReview from '@/models/TaskRecordReview'
+import TaskRecordReviewTutorial from '@/components/TaskRecordReviewTutorial'
 
-  export default {
-    name: 'TaskRecordReviewRetrieve',
-    components: {
-      AppBreadcrumb,
-      AppHeader,
-      AppWaiter,
-      TaskRecordReviewCard,
+export default {
+  name: 'TaskRecordReviewRetrieve',
+  components: {
+    AppBreadcrumb,
+    AppHeader,
+    AppWaiter,
+    TaskRecordReviewCard,
+    TaskRecordReviewTutorial
+
+  },
+  props: {
+    taskId: {
+      type: [Number, String]
     },
-    props: {
-      taskId: {
-        type: [Number, String]
-      },
-      taskRecordReviewId: {
-        type: [Number, String]
-      },
-    },
-    data () {
-      return {
-        resolvedTaskRecordReviewId: null
-      }
-    },
-    created () {
+    taskRecordReviewId: {
+      type: [Number, String]
+    }
+  },
+  data () {
+    return {
+      resolvedTaskRecordReviewId: null
+    }
+  },
+  created () {
+    return this.setup()
+  },
+  watch: {
+    taskId () {
       return this.setup()
     },
-    watch: {
-      taskId () {
-        return this.setup()
-      },
-      taskRecordReviewId () {
-        return this.setup()
+    taskRecordReviewId () {
+      return this.setup()
+    }
+  },
+  computed: {
+    fetchAllLoader () {
+      return uniqueId('load-task-record-review-retrieve-')
+    },
+    highlightNote () {
+      if (String(this.$route.query.highlightNote).toLowerCase() === 'true') {
+        return this.taskRecordReviewId
+      }
+      return false
+    },
+    task () {
+      return Task
+        .query()
+        .with('checkers')
+        .with('choiceGroup')
+        .with('choiceGroup.alternativeValues')
+        .with('choiceGroup.choices')
+        .find(this.taskId)
+    }
+  },
+  methods: {
+    async setup () {
+      try {
+        await this.waitFor(this.fetchAllLoader, this.fetchAll)
+        this.$core.setPageTitle(this.task.name)
+      } catch (error) {
+        const title = 'Unable to find this resource'
+        this.$router.replace({ name: 'error', params: { title, error } })
       }
     },
-    computed: {
-      fetchAllLoader () {
-        return uniqueId('load-task-record-review-retrieve-')
-      },
-      highlightNote () {
-        if (String(this.$route.query.highlightNote).toLowerCase() === 'true') {
-          return this.taskRecordReviewId
-        }
-        return false
-      },
-      task () {
-        return Task
-          .query()
-          .with('checkers')
-          .with('choiceGroup')
-          .with('choiceGroup.alternativeValues')
-          .with('choiceGroup.choices')
-          .find(this.taskId)
-      }
+    async fetchAll () {
+      await this.fetchTask()
+      await this.fetchChoiceGroup()
+      await this.fetchTaskRecordReview()
     },
-    methods: {
-      async setup () {
-        try {
-          await this.waitFor(this.fetchAllLoader, this.fetchAll)
-          this.$core.setPageTitle(this.task.name)
-        } catch (error) {
-          const title = 'Unable to find this resource'
-          this.$router.replace({ name: 'error', params: { title, error } })
-        }
-      },
-      async fetchAll () {
-        await this.fetchTask()
-        await this.fetchChoiceGroup()
-        await this.fetchTaskRecordReview()
-      },
-      async fetchTask () {
-        const params = { include: 'project,checkers' }
-        return Task.api().find(this.taskId, { params })
-      },
-      async fetchChoiceGroup () {
-        const params = { include: 'alternative_values,choices' }
-        return ChoiceGroup.api().find(this.task.choiceGroupId, { params })
-      },
-      async fetchTaskRecordReview () {
-        // User might try to access a TaskRecordReview assigned to another user.
-        // We need to find the id of the review assigned to the current user.
-        const params = { 'filter[task_record__reviews__id]': this.taskRecordReviewId }
-        const { response } = await TaskRecordReview.api().get('', { params })
-        const reviews = get(response, 'data.data', [])
-        const userReviewId = find(reviews, ({ id }) => TaskRecordReview.find(id)?.editable)?.id || null
-        // In case we didn't a task record review a user can edit, 
-        // we just use the first one from the list
-        const firstReviewId = reviews[0]?.id || null
-        this.resolvedTaskRecordReviewId = userReviewId || firstReviewId
-      },
-      async waitFor (loader, fn) {
-        this.$wait.start(loader)
-        await fn()
-        this.$wait.end(loader)
-      }
+    async fetchTask () {
+      const params = { include: 'project,checkers' }
+      return Task.api().find(this.taskId, { params })
+    },
+    async fetchChoiceGroup () {
+      const params = { include: 'alternative_values,choices' }
+      return ChoiceGroup.api().find(this.task.choiceGroupId, { params })
+    },
+    async fetchTaskRecordReview () {
+      // User might try to access a TaskRecordReview assigned to another user.
+      // We need to find the id of the review assigned to the current user.
+      const params = { 'filter[task_record__reviews__id]': this.taskRecordReviewId }
+      const { response } = await TaskRecordReview.api().get('', { params })
+      const reviews = get(response, 'data.data', [])
+      const userReviewId = find(reviews, ({ id }) => TaskRecordReview.find(id)?.editable)?.id || null
+      // In case we didn't a task record review a user can edit,
+      // we just use the first one from the list
+      const firstReviewId = reviews[0]?.id || null
+      this.resolvedTaskRecordReviewId = userReviewId || firstReviewId
+    },
+    async waitFor (loader, fn) {
+      this.$wait.start(loader)
+      await fn()
+      this.$wait.end(loader)
     }
   }
+}
 </script>
 
 <template>
@@ -122,8 +125,10 @@
       </app-breadcrumb>
       <app-header class="flex-grow-1" />
     </div>
+
     <div class="task-record-review-retrieve__container">
       <div class="container-fluid p-5">
+    <task-record-review-tutorial />
         <app-waiter :loader="fetchAllLoader" waiter-class="my-5 mx-auto d-block">
           <template v-if="resolvedTaskRecordReviewId">
             <task-record-review-card
