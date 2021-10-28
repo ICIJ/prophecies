3f78345e83c6598843f1593b1f830d80fb8344fe
Caroline Desprat <cdesprat@icij.org>
2021-10-28 09:32:03 +0000

    feat: add task stats card component to stat list page #65

--- prophecies/apps/frontend/src/views/StatsList.vue
+++ prophecies/apps/frontend/src/views/StatsList.vue
@@ -4,31 +4,90 @@
     <div class="stats-list__container flex-grow-1">
       <app-header reduced />
       <div class="container-fluid p-5">
-          hi!
+
+        <div class="d-flex align-items-center">
+          <b-form-group>
+            <b-form-radio-group
+              v-model="teamTaskStats"
+              buttons
+              button-variant="outline-primary"
+              :options="taskStatsOptions" />
+          </b-form-group>
+          <app-waiter :loader="fetchTaskLoader" waiter-class="my-5 mx-auto d-block">
+
+            <task-stats-card class="my-5"
+              v-for="task in tasks"
+              :key="task.id"
+              :team="teamTaskStats"
+              :task-id="task.id" />
+          </app-waiter>
+
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { uniqueId } from 'lodash'
+
 import AppHeader from '@/components/AppHeader.vue'
 import AppSidebar from '@/components/AppSidebar'
+import AppWaiter from '@/components/AppWaiter'
+
+import TaskStatsCard from '@/components/TaskStatsCard'
+import Task from '@/models/Task'
 export default {
   name: 'StatsList',
   components: {
     AppHeader,
-    AppSidebar
+    AppSidebar,
+    AppWaiter,
+    TaskStatsCard
   },
   props: {
 
   },
   data () {
     return {
-
+      teamTaskStats: true,
+      taskStatsOptions: [
+        { text: 'Team stats', value: true },
+        { text: 'Your stats', value: false }
+      ]
+    }
+  },
+  async created () {
+    await this.waitFor(this.fetchTaskLoader, this.fetchTask)
+  },
+  methods: {
+    fetchTask () {
+      return Task.api().get()
+    },
+    async waitFor (loader, fn) {
+      this.$wait.start(loader)
+      await fn()
+      this.$wait.end(loader)
     }
   },
   computed: {
-
+    tasks () {
+      return Task
+        .query()
+        .where('taskRecordsCount', (value) => value > 0)
+        .get()
+        .sort((a, b) => {
+          if (a.status === 'CLOSED') {
+            return 1
+          } else if (b.status === 'CLOSED') {
+            return -1
+          }
+          return 0
+        })
+    },
+    fetchTaskLoader () {
+      return uniqueId('load-dashboard-task-')
+    }
   }
 }
 </script>
