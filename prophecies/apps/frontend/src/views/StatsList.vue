852b94903f7c6e2e3958f0df0bc0edf6ab1d76a9
Caroline Desprat <cdesprat@icij.org>
2021-10-28 09:18:06 +0000

    feat: add stats list page template #65

--- prophecies/apps/frontend/src/views/StatsList.vue
+++ prophecies/apps/frontend/src/views/StatsList.vue
@@ -1,13 +1,23 @@
 <template>
-  <div class="stats-list">
-    hi!
+  <div class="stats-list d-flex align-items-start">
+   <app-sidebar class="w-100 sticky-top" />
+    <div class="stats-list__container flex-grow-1">
+      <app-header reduced />
+      <div class="container-fluid p-5">
+          hi!
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import AppHeader from '@/components/AppHeader.vue'
+import AppSidebar from '@/components/AppSidebar'
 export default {
   name: 'StatsList',
   components: {
+    AppHeader,
+    AppSidebar
   },
   props: {
 
