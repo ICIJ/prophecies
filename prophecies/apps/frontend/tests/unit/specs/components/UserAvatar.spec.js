1b6dd5479f101c366782ce76092b3f2a9240be27
Pierre Romera <hello@pirhoo.com>
2022-01-20 19:35:33 +0100

    test: add test for UserAvatar

---
+++ prophecies/apps/frontend/tests/unit/specs/components/UserAvatar.spec.js
@@ -0,0 +1,36 @@
+import { createLocalVue, mount } from '@vue/test-utils'
+import UserAvatar from '@/components/UserAvatar'
+import User from '@/models/User'
+import Core from '@/core'
+
+describe('UserAvatar', () => {
+  let wrapper
+
+  beforeAll(async () => {
+    User.insert({
+      data: { 
+        id: '2000',
+        username: 'Fátima',
+        firstName: 'Fátima',
+        lastName: 'Reinhardt',
+        email: 'engineering@icij.org',
+        emailMd5: '628e9a99d87799e9d434b63d2c3744ca',
+        isStaff: true
+      }
+    })
+  })
+
+  beforeEach(async () => {
+    const localVue = createLocalVue()
+    const propsData = { userId: '2000' }
+    // Configure the local vue
+    Core.init(localVue).useAll().configure()
+    // Finally, instanciate the component
+    wrapper = mount(UserAvatar, { localVue, propsData })
+  })
+
+  it('should build an avatar URL using the emailMd5', () => {
+    const src = wrapper.attributes('src')
+    expect(src).toBe('https://www.gravatar.com/avatar/628e9a99d87799e9d434b63d2c3744ca')
+  })
+})
