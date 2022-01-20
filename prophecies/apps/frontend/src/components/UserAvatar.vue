db7abd1a07ffad61061c8e5dee640c9cba843232
Pierre Romera <hello@pirhoo.com>
2022-01-20 17:24:43 +0100

    refactor: isolated userr avatar in a component

---
+++ prophecies/apps/frontend/src/components/UserAvatar.vue
@@ -0,0 +1,32 @@
+<script>
+  import { template } from 'lodash'
+  import User from '@/models/User'
+
+  export default {
+    name: 'UserAvatar',
+    props: {
+      userId: {
+        type: [String, Number]
+      },
+      size: {
+        type: [String, Number],
+        default: 42
+      }
+    },
+    computed: {
+      user () {
+        return User.find(this.userId)
+      },
+      userAvatarUrl () {
+        const interpolate = this.$config.get('templateInterpolate')
+        const avatarUrlTemplate = this.$config.get('avatarUrlTemplate')
+        const compiled = template(avatarUrlTemplate, { interpolate })
+        return compiled(this.user)
+      }
+    }
+  }
+</script>
+
+<template>
+  <img :src="userAvatarUrl" class="user-avatar rounded-circle" :height="size" :width="size" />
+</template>
