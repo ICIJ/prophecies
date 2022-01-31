2121eeef03292fb49c5262ae6f4b703afb3e7e99
Caroline Desprat <cdesprat@icij.org>
2022-01-31 14:28:52 +0000

    test: consolidate display of users

--- prophecies/apps/frontend/src/components/StatsByUsers.vue
+++ prophecies/apps/frontend/src/components/StatsByUsers.vue
@@ -1,31 +1,31 @@
 <template>
     <div class="stats-by-users d-flex flex-column flex-grow-1 py-3">
-      <div class="stats-by-users__table d-flex flex-row py-3" v-for="(user, index) in users" :key="index">
-        <div class="stats-by-users__table__username col-3 px-0 text-nowrap">
+      <div class="stats-by-users__row d-flex flex-row py-3" v-for="(user, index) in users" :key="index">
+        <div class="stats-by-users__row__username col-3 px-0 text-nowrap">
           {{ user.name }}
         </div>
-        <div class="stats-by-users__table__progress text-right mx-3">
-          <span class="stats-by-users__table__number">{{ user.progress | round}}</span>%
+        <div class="stats-by-users__row__progress text-right mx-3">
+          <span class="stats-by-users__row__number">{{ user.progress | round}}</span>%
         </div>
-        <div :class="tableClassList">
-          <check-icon size="1x" class="text-primary mr-2" /><span class="stats-by-users__table__number">{{user.done}}</span>
+        <div class="stats-by-users__row__done" :class="tableClassList">
+          <check-icon size="1x" class="text-primary mr-2" /><span class="stats-by-users__row__number">{{user.done}}</span>
         </div>
-        <div :class="tableClassList"  >
-          <clock-icon size="1x" class="text-danger mr-2" /><span class="stats-by-users__table__number">{{user.pending}}</span>
+        <div class="stats-by-users__row__pending" :class="tableClassList"  >
+          <clock-icon size="1x" class="text-danger mr-2" /><span class="stats-by-users__row__number">{{user.pending}}</span>
         </div>
       </div>
       <div class="stats-by-users__total d-flex flex-row py-2 font-weight-bold flex-grow-1 align-items-end" >
         <div class="col-3 pl-0">
           Total
         </div>
-        <div class="stats-by-users__table__progress text-right mx-3" >
-        <span class="stats-by-users__table__number">{{ totalStats.progress | round}}</span>%
+        <div class="stats-by-users__row__progress text-right mx-3" >
+        <span class="stats-by-users__row__number">{{ totalStats.progress | round}}</span>%
         </div>
         <div :class="tableClassList">
-          <check-icon size="1x" class="text-primary mr-2" /><span class="stats-by-users__table__number">{{ totalStats.done }}</span>
+          <check-icon size="1x" class="text-primary mr-2" /><span class="stats-by-users__row__number">{{ totalStats.done }}</span>
         </div>
         <div :class="tableClassList"  >
-          <clock-icon size="1x" class="text-danger mr-2" /><span class="stats-by-users__table__number">{{ totalStats.pending }}</span>
+          <clock-icon size="1x" class="text-danger mr-2" /><span class="stats-by-users__row__number">{{ totalStats.pending }}</span>
         </div>
       </div>
     </div>
@@ -41,16 +41,21 @@ export default {
       default: () => ([])
     }
   },
-  computed: {
-    tableClassList () {
-      return 'd-flex px-3 text-nowrap stats-by-users__table__cell__icon '
-    },
-    totalStats () {
-      return this.users.reduce((acc, user) => ({
+  methods: {
+    computeStats (users) {
+      return users.reduce((acc, user) => ({
         done: acc.done + user.done,
         pending: acc.pending + user.pending,
-        progress: acc.progress + user.progress
+        progress: acc.progress + user.progress / users.length
       }), { done: 0, pending: 0, progress: 0 })
+    }
+  },
+  computed: {
+    tableClassList () {
+      return 'd-flex px-3 text-nowrap stats-by-users__row__cell__icon '
+    },
+    totalStats () {
+      return this.computeStats(this.users)
     }
   },
   filters: {
@@ -61,7 +66,7 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-.stats-by-users__table
+.stats-by-users__row
 {
   &__number{
     font-variant-numeric: tabular-nums;
---
+++ prophecies/apps/frontend/tests/unit/specs/components/StatsByUsers.spec.js
@@ -0,0 +1,44 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import StatsByUsers from '@/components/StatsByUsers'
+import Core from '@/core'
+
+describe('StatsByUsers', () => {
+  describe('Show list of users and the total', () => {
+    let wrapper
+    beforeEach(async () => {
+      const localVue = createLocalVue()
+      const propsData = { users: [{ name: 'django', done: 5, pending: 0, progress: 100 }, { name: 'olivia', done: 2, pending: 2, progress: 50 }] }
+      // Configure the local vue
+      const core = Core.init(localVue).useAll()
+      // Configure the core
+      await core.configure()
+      // Get router from core
+      const { i18n } = core
+      // Finally, instanciate the component
+      wrapper = shallowMount(StatsByUsers, { localVue, propsData, i18n })
+    })
+
+    it('should show two users stats', () => {
+      const elements = wrapper.findAll('.stats-by-users__row')
+      expect(elements).toHaveLength(2)
+    })
+    it('should display user name', () => {
+      const element = wrapper.find('.stats-by-users__row__username')
+      expect(element.text()).toEqual('django')
+    })
+
+    it('should display number of done reviews with the check icon', () => {
+      const element = wrapper.find('.stats-by-users__row__done')
+      expect(element.text()).toEqual('5')
+      const checkIcon = element.find('check-icon-stub')
+      expect(checkIcon.exists()).toBeTruthy()
+    })
+    it('computes the stats of given users', () => {
+      const users = [{ name: 'django', done: 5, pending: 0, progress: 100 }, { name: 'olivia', done: 2, pending: 2, progress: 50 }]
+      const total = wrapper.vm.computeStats(users)
+      expect(total.done).toEqual(7)
+      expect(total.pending).toEqual(2)
+      expect(total.progress).toEqual(75)
+    })
+  })
+})
