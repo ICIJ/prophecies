8cab9291c639b10fb3dedbf394dcf19537b0c20e
Caroline Desprat <cdesprat@icij.org>
2022-02-09 11:04:40 +0000

    refactor: update layout for flexibilty

--- prophecies/apps/frontend/src/components/TaskStatsCardAllRounds.vue
+++ prophecies/apps/frontend/src/components/TaskStatsCardAllRounds.vue
@@ -21,51 +21,75 @@ export default {
 </script>
 
 <template>
-  <div class="task-stats-card-all-rounds card card-body rounded ">
-    <div class="d-flex flex-row">
-      <div class="col-1 task-stats-card-all-rounds__all text-right px-0">
-        All
+  <b-container class="task-stats-card-all-rounds card card-body rounded font-weight-bold">
+    <b-row class="pt-2">
+      <div
+        class="
+          task-stats-card-all-rounds__all
+          text-center
+          text-primary
+          col-2
+          px-0
+        "
+      >
+        <slot name="title">All</slot>
       </div>
-        <div class="col-11 task-stats-card-all-rounds__progress py-2">
-          <b-progress :value="progress | round" :max="100" />
-        </div>
-    </div>
-    <div class="d-flex flex-row font-weight-bold text-center ">
-      <div class="offset-1 col-3 px-0">
-        <span class=" p-1
-          bg-primary
-          text-white
-          rounded" >
-          {{ progress | round }}%
-        </span>
-      </div>
-      <div class="col-4 px-0">
-        <check-icon size="1x" class="text-primary mr-2" />  {{done}}
-      </div>
-      <div class="col-4  px-0">
-        <clock-icon size="1x" class="text-danger mr-2" />  {{pending}}
-      </div>
-
-    </div>
-  </div>
+      <b-col class="task-stats-card-all-rounds__stats pr-0">
+        <b-row
+          class="
+            task-stats-card-all-rounds__stats__progress-bar
+            col-12
+            p-0
+            mx-0
+            py-2
+          "
+        >
+          <b-progress class="col-12 p-0" :value="progress | round" :max="100" />
+        </b-row>
+        <b-row class="mt-auto">
+          <b-col class="pr-0">
+            <span
+              class="
+                task-stats-card-all-rounds__stats__progress
+                bg-primary
+                text-white
+                text-center
+                rounded
+                d-inline-block
+              "
+            >
+              {{ progress | round }}%
+            </span>
+          </b-col>
+          <b-col class="pr-0">
+            <check-icon size="1x" class="text-primary mr-2" /> {{ done }}
+          </b-col>
+          <b-col class="pr-0">
+            <clock-icon size="1x" class="text-danger mr-2" /> {{ pending }}
+          </b-col>
+        </b-row>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <style lang="scss" scoped>
-  .task-stats-card-all-rounds {
-      padding-left: 48px;
-      padding-right: 48px;
-      width:340px;
-      min-width:340px;
-      max-width:340px;
-      background-color: $secondary-50;
+.task-stats-card-all-rounds {
+  padding-left: $spacer-xxl;
+  padding-right: $spacer-xxl;
+  min-width: 340px;
+  max-width: 360px;
+  max-height: 93px;
+  background-color: $secondary-50;
 
-    &__all {
-      color: $primary;
-      font-weight: bold;
-    }
-
-    &__progress {
+  &__stats {
+    &__progress-bar {
       height: 2em;
+      width: 100%;
+    }
+    &__progress {
+      width: 45px;
     }
   }
+}
 </style>
