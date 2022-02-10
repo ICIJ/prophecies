a206865061b95c05154f1ed59164cf32b4346e90
Caroline Desprat <cdesprat@icij.org>
2022-02-10 15:10:26 +0000

    refactor: move task sorting to sort file

--- prophecies/apps/frontend/src/utils/sort.js
+++ prophecies/apps/frontend/src/utils/sort.js
@@ -1,3 +1,6 @@
+import { TaskStatusOrder } from '@/models/Task'
+import { orderBy } from 'lodash'
+
 export function sortByProjectThenTask (a, b) {
   if (!a.project) { return -1 }
   if (!b.project) { return 1 }
@@ -9,4 +12,9 @@ export function sortByProjectThenTask (a, b) {
     if (!b.task) { return 1 }
     return (a.task?.name?.localeCompare(b.task?.name))
   }
+}
+
+export const orderTasks = (tasks) => {
+  const orderByStatus = task => TaskStatusOrder[task.status] === 1
+  return orderBy(tasks, [orderByStatus, 'priority', 'name'])
 }
--- prophecies/apps/frontend/tests/unit/specs/utils/sort.spec.js
+++ prophecies/apps/frontend/tests/unit/specs/utils/sort.spec.js
@@ -1,24 +1,34 @@
 import '@/store'
-import { sortByProjectThenTask } from '@/utils/sort'
+import { sortByProjectThenTask, orderTasks } from '@/utils/sort'
 import Tip from '@/models/Tip'
+import Task from '@/models/Task'
 
 describe('utils/sort', () => {
   beforeAll(async () => {
     await Tip.api().get()
+    await Task.api().get()
   })
 
   afterAll(() => {
     Tip.deleteAll()
+    Task.deleteAll()
   })
 
   describe('sortByProjectThenTask', () => {
+    beforeAll(async () => {
+      await Tip.api().get()
+    })
+
+    afterAll(() => {
+      Tip.deleteAll()
+    })
     it('shouldn\'t sort when project and task aren\'t available', () => {
-      var list = Tip.all()
-      var sorted = list.sort(sortByProjectThenTask)
+      const list = Tip.all()
+      const sorted = list.sort(sortByProjectThenTask)
       expect(list).toBe(sorted)
     })
     it('should sort elements by project', () => {
-      var sorted = Tip.query().with('project').get().sort(sortByProjectThenTask)
+      const sorted = Tip.query().with('project').get().sort(sortByProjectThenTask)
       expect(sorted[0].project).toBeNull()
       expect(sorted[1].project.name).toBe('Aladdin')
       expect(sorted[2].project.name).toBe('Aladdin')
@@ -29,7 +39,7 @@ describe('utils/sort', () => {
     })
 
     it('should sort elements project then by task if available', () => {
-      var sorted = Tip.query().with('project').with('task').get().sort(sortByProjectThenTask)
+      const sorted = Tip.query().with('project').with('task').get().sort(sortByProjectThenTask)
       expect(sorted[1].project.name).toBe('Aladdin')
       expect(sorted[1].task.name).toBe('Addresses')
       expect(sorted[2].project.name).toBe('Aladdin')
@@ -39,6 +49,35 @@ describe('utils/sort', () => {
       expect(sorted[4].task).toBeNull()
       expect(sorted[4].project.name).toBe('Chronos')
       expect(sorted[5].task.name).toBe('Shops')
+    })
+  })
+  describe('sort tasks ', () => {
+    beforeAll(async () => {
+      await Task.api().get()
+    })
+
+    afterAll(() => {
+      Task.deleteAll()
+    })
+    it('from open to close', () => {
+      const sorted = orderTasks(Task.all())
+      expect(sorted).toHaveLength(5)
+
+      let task = sorted[0]
+      expect(task.status).toEqual('LOCKED')
+      expect(task.name).toEqual('Addresses')
+      task = sorted[1]
+      expect(task.status).toEqual('OPEN')
+      expect(task.name).toEqual('Cats')
+      task = sorted[2]
+      expect(task.status).toEqual('OPEN')
+      expect(task.name).toEqual('Phones')
+      task = sorted[3]
+      expect(task.status).toEqual('OPEN')
+      expect(task.name).toEqual('Shops')
+      task = sorted[4]
+      expect(task.status).toEqual('CLOSED')
+      expect(task.name).toEqual('Immatriculations')
     })
   })
 })
