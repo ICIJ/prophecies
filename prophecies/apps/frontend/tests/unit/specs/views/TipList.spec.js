bcbf49185dc5aaefdf0532903b008d63b2a2e991
Pierre Romera <hello@pirhoo.com>
2022-02-14 17:01:35 +0100

    refactor: lint style

--- prophecies/apps/frontend/tests/unit/specs/views/TipList.spec.js
+++ prophecies/apps/frontend/tests/unit/specs/views/TipList.spec.js
@@ -6,26 +6,25 @@ import User from '@/models/User'
 import TipList from '@/views/TipList'
 import TipCard from '@/components/TipCard'
 
-describe('Tip list', () => {
+describe('Tiplist', () => {
   let wrapper
+
   function createContainer () {
     const div = document.createElement('div')
     document.body.appendChild(div)
     return div
   }
+
   beforeAll(async () => {
     await User.api().get()
   })
 
   beforeEach(async () => {
     const attachTo = createContainer()
-
     const localVue = createLocalVue()
     // Configure the local vue with plugins
-    const stubs = ['app-sidebar', 'app-header', 'latest-tips-card', 'tip-list-page-params']
-
+    const stubs = ['app-sidebar', 'app-waiter', 'app-header', 'latest-tips-card', 'tip-list-page-params']
     const { i18n, wait, store, router } = Core.init(localVue).useAll()
-
     wrapper = mount(TipList, {
       attachTo,
       i18n,
@@ -46,12 +45,13 @@ describe('Tip list', () => {
   // })
 
   it('should display list of tips', () => {
-    const tipitems = wrapper.findAllComponents(TipCard)
-    expect(tipitems).toHaveLength(7)
+    const tipItems = wrapper.findAllComponents(TipCard)
+    expect(tipItems).toHaveLength(7)
   })
+
   it('filter tip list on project id 1', async () => {
     await wrapper.setData({ query: { 'filter[project]': '1' } }) // aladdin project id
-    const tipitems = wrapper.findAllComponents(TipCard)
-    expect(tipitems).toHaveLength(3)
+    const tipItems = wrapper.findAllComponents(TipCard)
+    expect(tipItems).toHaveLength(3)
   })
 })
