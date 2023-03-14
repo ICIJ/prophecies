059d6720359232fe1346827685a93d7d35f78a27
Bruno Thomas <bthomas@icij.org>
2023-03-14 11:12:47 +0000

    chore: lint change variable rules & fix core/oauth_provider.py

--- prophecies/core/forms/setting_form.py
+++ prophecies/core/forms/setting_form.py
@@ -1,16 +1,15 @@
-from django import forms
-from django.forms import fields
-from constance.admin import ConstanceForm
 from constance import settings
+from constance.admin import ConstanceForm
+from django import forms
 
 from prophecies.core.models import Setting
 
 
 class SettingForm(ConstanceForm):
-
+    # pylint: disable=keyword-arg-before-vararg
     def __init__(self, initial, request=None, *args, **kwargs):
         super().__init__(initial=initial, request=request, *args, **kwargs)
-        # One boolean field is created for each setting 
+        # One boolean field is created for each setting
         for name in settings.CONFIG:
             field_name = SettingForm.visibility_field_name(name)
             self.fields[field_name] = forms.BooleanField(required=False)
@@ -21,11 +20,11 @@ class SettingForm(ConstanceForm):
 
     @staticmethod
     def visibility_field_name(name):
-        return '%s_visibility' % name
+        return f'{name}_visibility'
 
     def save(self):
         for name in settings.CONFIG:
-            setting, created = Setting.objects.get_or_create(key=name)
+            setting, _created = Setting.objects.get_or_create(key=name)
             setting.public = self.cleaned_data.get(SettingForm.visibility_field_name(name), False)
             setting.save()
         super().save()
