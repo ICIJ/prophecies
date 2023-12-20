f24ce94dbe3b8b0d835613c9c62722ee8963fcb1
Pierre Romera <hello@pirhoo.com>
2023-12-20 15:19:50 +0100

    feat: add task record media upload form

--- prophecies/core/forms/__init__.py
+++ prophecies/core/forms/__init__.py
@@ -2,4 +2,5 @@ from .abstract_upload_form import AbstractUploadForm
 from .alternative_value_upload_form import AlternativeValueUploadForm
 from .task_record_assign_form import TaskRecordAssignForm
 from .task_record_upload_form import TaskRecordUploadForm
+from .task_record_media_upload_form import TaskRecordMediaUploadForm
 from .setting_form import SettingForm
