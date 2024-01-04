6d229e1d6cfbfe4b14c6f24d07f69d505a505cd8
Pierre Romera <hello@pirhoo.com>
2024-01-04 16:26:17 +0100

    feat: add haptic copy button component with clipboard icon

---
+++ prophecies/apps/frontend/src/components/HapticCopyButton.vue
@@ -0,0 +1,60 @@
+<script>
+export default {
+  name: 'HapticCopyButton',
+  props: {
+    text: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<template>
+  <haptic-copy
+    :text="text"
+    class="haptic-copy-button font-weight-bold"
+    tooltip-placement="right"
+    v-b-tooltip.hover.right="'Click to copy'">
+    <template v-if="$slots.default">
+      <slot />
+    </template>
+    <template v-else>
+      {{ text }}
+    </template>
+    <span class="haptic-copy-button__clipboard ml-1">
+      <clipboard-icon />
+    </span>
+  </haptic-copy>
+</template>
+
+<style lang="scss" scoped>
+.haptic-copy-button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: $spacer-xs $spacer;
+
+  &:hover {
+    background: $primary-10;
+  }
+
+  &__clipboard {
+    min-width: 1.2rem;
+    align-self: center;
+    color: $tertiary;
+    opacity: 0;
+    transition: $btn-transition, opacity .15s;
+
+    .feather {
+      height: 1.5em;
+    }
+  }
+
+  &:focus &__clipboard,
+  &:active &__clipboard,
+  &:hover &__clipboard {
+    opacity: 1;
+  }
+}
+</style>
