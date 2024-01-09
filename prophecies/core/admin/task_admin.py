51579c718a09c6c144377d530e2c75383e9bc99a
Pierre Romera <hello@pirhoo.com>
2024-01-09 18:57:43 +0100

    feat: add inline models to handle task template settings

--- prophecies/core/admin/task_admin.py
+++ prophecies/core/admin/task_admin.py
@@ -3,29 +3,64 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.utils.html import format_html
+from polymorphic.admin import PolymorphicInlineSupportMixin, StackedPolymorphicInline
 from prophecies.core.contrib.display import display_task_addon
 from prophecies.core.forms.task_form import TaskForm
 from prophecies.core.models import Task
+from prophecies.core.models.task_template_setting import (
+    TaskTemplateSetting,
+    TaskTemplateSettingForText,
+    TaskTemplateSettingForMedia,
+    TaskTemplateSettingForIframe,
+)
+
+
+class TaskTemplateSettingInline(StackedPolymorphicInline):
+    class TaskTemplateSettingForTextInline(StackedPolymorphicInline.Child):
+        model = TaskTemplateSettingForText
+
+    class TaskTemplateSettingForMediaInline(StackedPolymorphicInline.Child):
+        model = TaskTemplateSettingForMedia
+
+    class TaskTemplateSettingForIframeInline(StackedPolymorphicInline.Child):
+        model = TaskTemplateSettingForIframe
+
+    model = TaskTemplateSetting
+    can_delete = True
+    verbose_name = "Template Setting"
+    max_num = 1
+    child_inlines = (
+        TaskTemplateSettingForTextInline,
+        TaskTemplateSettingForMediaInline,
+        TaskTemplateSettingForIframeInline,
+    )
 
 
 class TaskAdminForm(TaskForm):
-    checkers = forms.ModelMultipleChoiceField(required=True, queryset=User.objects.all(),
-                                              widget=forms.CheckboxSelectMultiple)
+    checkers = forms.ModelMultipleChoiceField(
+        required=True, queryset=User.objects.all(), widget=forms.CheckboxSelectMultiple
+    )
 
 
 @admin.register(Task)
-class TaskAdmin(admin.ModelAdmin):
-    exclude = ['creator']
-    search_fields = ['name']
-    list_display = ('task_with_addon', 'description', 'task_actions', 'task_status',)
+class TaskAdmin(PolymorphicInlineSupportMixin, admin.ModelAdmin):
+    exclude = ["creator"]
+    search_fields = ["name"]
+    list_display = (
+        "task_with_addon",
+        "description",
+        "task_actions",
+        "task_status",
+    )
     form = TaskAdminForm
+    inlines = [TaskTemplateSettingInline]
 
     def task_actions(self, obj):
-        path = reverse('admin:core_taskrecord_upload')
-        url = f'{path}?task={obj.id}'
+        path = reverse("admin:core_taskrecord_upload")
+        url = f"{path}?task={obj.id}"
         return format_html('<a href="{}">Upload records</a>', url)
 
-    task_actions.short_description = 'Actions'
+    task_actions.short_description = "Actions"
 
     def task_status(self, task):
         return task.status.capitalize()
@@ -38,6 +73,18 @@ class TaskAdmin(admin.ModelAdmin):
     task_with_addon.short_description = "Task Name"
 
     def save_model(self, request, obj, form, change):
+        template_setting, created = self.get_or_create_template_setting(obj)
+        if created:
+            obj.template_setting.set([template_setting])
+
         if not obj.creator:
             obj.creator = request.user
         super().save_model(request, obj, form, change)
+
+    def get_or_create_template_setting(self, obj):
+        template_setting = obj.template_setting.first()
+        if template_setting is None:
+            return obj.template_type_model.objects.get_or_create(task=obj)
+        elif not isinstance(template_setting, obj.template_type_model):
+            return template_setting.convert_or_create(obj.template_type_model)
+        return template_setting, False
--- prophecies/core/models/task.py
+++ prophecies/core/models/task.py
@@ -1,5 +1,6 @@
 from actstream import action
 from colorfield.fields import ColorField
+from django.apps import apps
 from django.core.cache import cache
 from django.contrib.auth.models import User
 from django.db import models
@@ -13,24 +14,24 @@ from prophecies.core.contrib.colors import hex_scale_brightness
 
 
 class StatusType(models.TextChoices):
-    OPEN = 'OPEN', _('Open')
-    CLOSED = 'CLOSED', _('Closed')
-    LOCKED = 'LOCKED', _('Locked')
+    OPEN = "OPEN", _("Open")
+    CLOSED = "CLOSED", _("Closed")
+    LOCKED = "LOCKED", _("Locked")
+
 
 class TemplateType(models.TextChoices):
     TEXT = "TEXT", _("Text Task")
     MEDIA = "MEDIA", _("Media Task")
+    IFRAME = "IFRAME", _("Iframe Task")
 
 
 class TaskQuerySet(models.QuerySet):
-
     def user_scope(self, user):
-        projects = self.filter(checkers=user).values('project')
+        projects = self.filter(checkers=user).values("project")
         return self.filter(project__in=projects)
 
 
 class TaskManager(models.Manager):
-
     def user_scope(self, user):
         return self.get_queryset().user_scope(user)
 
@@ -43,38 +44,78 @@ class Task(models.Model):
 
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
-    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="tasks",
-                                help_text="Project this task belong to")
-    checkers = models.ManyToManyField(User, through='TaskChecker', through_fields=('task', 'checker'),
-                                      verbose_name="User in charge of checking this task's data", related_name='task')
-    creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='tasks')
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name="tasks",
+        help_text="Project this task belong to",
+    )
+    checkers = models.ManyToManyField(
+        User,
+        through="TaskChecker",
+        through_fields=("task", "checker"),
+        verbose_name="User in charge of checking this task's data",
+        related_name="task",
+    )
+    creator = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True, related_name="tasks"
+    )
     rounds = models.PositiveIntegerField(default=3, verbose_name="Number of rounds")
-    automatic_round_attributions = models.BooleanField(default=False,
-                                                       verbose_name="Attribute rounds (if not checked, "
-                                                                    "all checkers will participate in all rounds)")
-    allow_multiple_checks = models.BooleanField(default=False,
-                                                verbose_name="Allow checkers to check several time the same item")
-    allow_items_addition = models.BooleanField(default=False,
-                                               verbose_name="Allow checker to add items (not implemented yet)")
+    automatic_round_attributions = models.BooleanField(
+        default=False,
+        verbose_name="Attribute rounds (if not checked, "
+        "all checkers will participate in all rounds)",
+    )
+    allow_multiple_checks = models.BooleanField(
+        default=False, verbose_name="Allow checkers to check several time the same item"
+    )
+    allow_items_addition = models.BooleanField(
+        default=False, verbose_name="Allow checker to add items (not implemented yet)"
+    )
     priority = models.PositiveIntegerField(default=1, verbose_name="Priority")
-    choice_group = models.ForeignKey(ChoiceGroup, verbose_name="Choices", on_delete=models.RESTRICT, null=True,
-                                     blank=True)
-    color = ColorField(default='#31807D')
-    record_link_template = models.CharField(max_length=1000, null=True, blank=True, verbose_name="Record link template",
-                                            help_text="A link template to build a link for each task record. "
-                                                      "Task record can override this value with their own link")
-    embeddable_links = models.BooleanField(default=False,
-                                           verbose_name="Allow end-users to preview links within an iframe "
-                                                        "(targeted website must allow it)")
-    embeddable_record_link_template = models.CharField(max_length=1000, null=True, blank=True,
-                                                       verbose_name="Embeddable record link template",
-                                                       help_text="An optional alternative link template "
-                                                                 "to use within the link preview.")
-    status = models.CharField(blank=True, choices=StatusType.choices, default=StatusType.OPEN, max_length=6,
-                              help_text="Status of the task. Set to closed or locked will prevent "
-                                        "any update of the records.")
-    template_type = models.CharField(max_length=16, choices=TemplateType.choices, default=TemplateType.TEXT,
-                                    help_text="Template type to use to display the task.")
+    choice_group = models.ForeignKey(
+        ChoiceGroup,
+        verbose_name="Choices",
+        on_delete=models.RESTRICT,
+        null=True,
+        blank=True,
+    )
+    color = ColorField(default="#31807D")
+    record_link_template = models.CharField(
+        max_length=1000,
+        null=True,
+        blank=True,
+        verbose_name="Record link template",
+        help_text="A link template to build a link for each task record. "
+        "Task record can override this value with their own link",
+    )
+    embeddable_links = models.BooleanField(
+        default=False,
+        verbose_name="Allow end-users to preview links within an iframe "
+        "(targeted website must allow it)",
+    )
+    embeddable_record_link_template = models.CharField(
+        max_length=1000,
+        null=True,
+        blank=True,
+        verbose_name="Embeddable record link template",
+        help_text="An optional alternative link template "
+        "to use within the link preview.",
+    )
+    status = models.CharField(
+        blank=True,
+        choices=StatusType.choices,
+        default=StatusType.OPEN,
+        max_length=6,
+        help_text="Status of the task. Set to closed or locked will prevent "
+        "any update of the records.",
+    )
+    template_type = models.CharField(
+        max_length=16,
+        choices=TemplateType.choices,
+        default=TemplateType.TEXT,
+        help_text="Template type to use to display the task.",
+    )
     created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
@@ -84,15 +125,21 @@ class Task(models.Model):
     def cache_key(self):
         app_label = self._meta.app_label
         model_name = self._meta.model_name
-        return f'{app_label}:{model_name}:{self.pk}'
+        return f"{app_label}:{model_name}:{self.pk}"
 
     @property
     def progress(self):
         cache_key = self.progress_cache_key()
         progress = cache.get(cache_key)
         if progress is None:
-            from prophecies.core.models.task_record_review import TaskRecordReview, StatusType as st
-            tasks_reviews = TaskRecordReview.objects.filter(task_record__task_id=self.id)
+            from prophecies.core.models.task_record_review import (
+                TaskRecordReview,
+                StatusType as st,
+            )
+
+            tasks_reviews = TaskRecordReview.objects.filter(
+                task_record__task_id=self.id
+            )
             done_reviews = tasks_reviews.filter(status=st.DONE)
             all_reviews = len(tasks_reviews)
             done_reviews = len(done_reviews)
@@ -101,13 +148,14 @@ class Task(models.Model):
         return progress
 
     def progress_cache_key(self):
-        return f'{self.cache_key}:progress'
+        return f"{self.cache_key}:progress"
 
     def progress_by_round(self, **task_record_review_filter):
         cache_key = self.progress_by_round_cache_key(**task_record_review_filter)
         progress = cache.get(cache_key)
         if progress is None:
             from prophecies.core.models.task_record_review import TaskRecordReview
+
             filter = dict(task_record__task=self, **task_record_review_filter)
             progress = TaskRecordReview.objects.progress_by_round(**filter)
             # Get all task's rounds to only display the progress by existing rounds
@@ -118,24 +166,26 @@ class Task(models.Model):
 
     def progress_by_round_cache_key(self, **task_record_review_filter):
         hash_key = hash(frozenset(task_record_review_filter.items()))
-        return f'{self.cache_key}:progress_by_round:{hash_key}'
+        return f"{self.cache_key}:progress_by_round:{hash_key}"
 
     @property
     def records_done_count(self):
-        cache_key = f'{self.cache_key}:records_done_count'
+        cache_key = f"{self.cache_key}:records_done_count"
         count = cache.get(cache_key)
         if count is None:
             from prophecies.core.models.task_record_review import TaskRecord
+
             count = TaskRecord.objects.done().filter(task=self).count()
             cache.set(cache_key, count, timeout=60)
         return count
 
     @property
     def records_count(self):
-        cache_key = f'{self.cache_key}:records_count'
+        cache_key = f"{self.cache_key}:records_count"
         count = cache.get(cache_key)
         if count is None:
             from prophecies.core.models.task_record_review import TaskRecord
+
             count = TaskRecord.objects.filter(task=self).count()
             cache.set(cache_key, count, timeout=60)
         return count
@@ -168,20 +218,31 @@ class Task(models.Model):
     def has_attribute_changed(current_instance, attribute_name):
         if current_instance.pk is not None:
             instance = Task.objects.get(pk=current_instance.pk)
-            return getattr(current_instance, attribute_name) != getattr(instance, attribute_name)
+            return getattr(current_instance, attribute_name) != getattr(
+                instance, attribute_name
+            )
         return False
 
     @property
     def is_open_changed(self):
-        return Task.has_attribute_changed(self, 'is_open')
+        return Task.has_attribute_changed(self, "is_open")
 
     @property
     def is_closed_changed(self):
-        return Task.has_attribute_changed(self, 'is_closed')
+        return Task.has_attribute_changed(self, "is_closed")
 
     @property
     def is_locked_changed(self):
-        return Task.has_attribute_changed(self, 'is_locked')
+        return Task.has_attribute_changed(self, "is_locked")
+
+    @property
+    def template_type_model(self):
+        model_mapping = {
+            TemplateType.TEXT: apps.get_model("core", "TaskTemplateSettingForText"),
+            TemplateType.MEDIA: apps.get_model("core", "TaskTemplateSettingForMedia"),
+            TemplateType.IFRAME: apps.get_model("core", "TaskTemplateSettingForIframe"),
+        }
+        return model_mapping[self.template_type]
 
     @cached_property
     def colors(self):
@@ -192,22 +253,28 @@ class Task(models.Model):
         return tuple(hex_scale_brightness(self.color, s) for s in scales)
 
     @staticmethod
-    def signal_log_task_locked(sender, instance, **kwargs):  # pylint: disable=unused-argument
+    def signal_log_task_locked(
+        sender, instance, **kwargs
+    ):  # pylint: disable=unused-argument
         if instance.is_locked_changed and instance.is_locked:
             if instance.creator:
-                action.send(instance.creator, verb='locked', target=instance)
+                action.send(instance.creator, verb="locked", target=instance)
 
     @staticmethod
-    def signal_log_task_closed(sender, instance, **kwargs):  # pylint: disable=unused-argument
+    def signal_log_task_closed(
+        sender, instance, **kwargs
+    ):  # pylint: disable=unused-argument
         if instance.is_closed_changed and instance.is_closed:
             if instance.creator:
-                action.send(instance.creator, verb='closed', target=instance)
+                action.send(instance.creator, verb="closed", target=instance)
 
     @staticmethod
-    def signal_log_task_open(sender, instance, **kwargs):  # pylint: disable=unused-argument
+    def signal_log_task_open(
+        sender, instance, **kwargs
+    ):  # pylint: disable=unused-argument
         if instance.is_open_changed and instance.is_open:
             if instance.creator:
-                action.send(instance.creator, verb='open', target=instance)
+                action.send(instance.creator, verb="open", target=instance)
 
 
 signals.pre_save.connect(Task.signal_log_task_locked, sender=Task)
