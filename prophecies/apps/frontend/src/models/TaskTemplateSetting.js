ce1877fd9984b143b622b81cc3db16a11a0806fe
Pierre Romera <hello@pirhoo.com>
2024-01-15 12:57:03 +0100

    feat: add TaskTemplateSetting models to the front

--- prophecies/apps/frontend/src/models/TaskTemplateSetting.js
+++ prophecies/apps/frontend/src/models/TaskTemplateSetting.js
@@ -1,16 +1,13 @@
 import { Model } from '@vuex-orm/core'
 import { responseNormalizer } from '@/utils/jsonapi'
 import settings from '@/settings'
-import Task from '@/models/Task'
 
 export default class TaskTemplateSetting extends Model {
   static entity = 'TaskTemplateSetting'
 
   static fields () {
     return {
-      id: this.attr(),
-      task: this.belongsTo(Task, 'taskId'),
-      taskId: this.attr(null),
+      id: this.attr()
     }
   }
 
