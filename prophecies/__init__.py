868e8364f6d7e76526e4a13bb54191a05a7ea9f6
Pierre Romera <hello@pirhoo.com>
2024-01-17 11:26:43 +0000

    refactor: indent

--- prophecies/__init__.py
+++ prophecies/__init__.py
@@ -1,8 +1,8 @@
 import pkg_resources
 
 try:
-  __version__ = pkg_resources.get_distribution('prophecies').version
+    __version__ = pkg_resources.get_distribution('prophecies').version
 except pkg_resources.DistributionNotFound:
-  __version__ = '0.0.0'
+    __version__ = '0.0.0'
 
 VERSION = __version__
