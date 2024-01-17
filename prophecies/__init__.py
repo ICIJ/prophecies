0aa3a8b0653622e304a6ced86c857fae96b2fd6a
Pierre Romera <hello@pirhoo.com>
2024-01-17 12:07:44 +0100

    feat: catch missing distribution error

--- prophecies/__init__.py
+++ prophecies/__init__.py
@@ -1,4 +1,8 @@
 import pkg_resources
 
-__version__ = pkg_resources.get_distribution('prophecies').version
+try:
+  __version__ = pkg_resources.get_distribution('prophecies').version
+except pkg_resources.DistributionNotFound:
+  __version__ = '0.0.0'
+
 VERSION = __version__
