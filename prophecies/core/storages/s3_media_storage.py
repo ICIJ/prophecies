ad4db586ee88c37fce116282068ecab0fe581ebf
Pierre Romera <hello@pirhoo.com>
2024-01-18 15:17:30 +0000

    refactor: disable abstract-method lint rule

--- prophecies/core/storages/s3_media_storage.py
+++ prophecies/core/storages/s3_media_storage.py
@@ -1,5 +1,7 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
+
+# pylint: disable=abstract-method
 class S3MediaStorage(S3Boto3Storage):
     location = "private"
     default_acl = "private"
