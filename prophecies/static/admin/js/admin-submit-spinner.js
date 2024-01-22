3793f1b37eafd8c2e2c0da9ccdfc4a37bcbded77
Pierre Romera <hello@pirhoo.com>
2024-01-22 13:18:36 +0100

    fix: assync input freezing

--- prophecies/static/admin/js/admin-submit-spinner.js
+++ prophecies/static/admin/js/admin-submit-spinner.js
@@ -15,31 +15,37 @@
   document.addEventListener('DOMContentLoaded', function () {
     document.querySelectorAll('form').forEach(form => {
       form.addEventListener('submit', function () {
-        // Freeze all submit buttons
-        this.querySelectorAll('[type=submit]').forEach(submitButton => {
-          submitButton.disabled = true
-        })
+        // Wait for the data to be actually submitted by the form
+        // to avoid stripping out data. It is because the HTML5 spec is ignoring
+        // disabled fields when submitting the data.
+        // @see https://html.spec.whatwg.org/multipage/form-control-infrastructure.html#constructing-the-form-data-set
+        setTimeout(() => {
+          // Freeze all submit buttons
+          this.querySelectorAll('[type=submit]').forEach(submitButton => {
+            submitButton.disabled = true
+          })
 
-        // Get the submitting target
-        const activeElement = document.activeElement
-        const isSubmitButton = activeElement.tagName === 'BUTTON' && activeElement.type === 'submit'
-        const isInSubmitRow = !!activeElement.closest('.submit-row')
+          // Get the submitting target
+          const activeElement = document.activeElement
+          const isSubmitButton = activeElement.tagName === 'BUTTON' && activeElement.type === 'submit'
+          const isInSubmitRow = !!activeElement.closest('.submit-row')
 
-        // In the submit row (change form),
-        // the button will be wrapped and
-        // the spinner put in absolute position.
-        if (isInSubmitRow) {
-          const wrapper = document.createElement('span')
-          wrapper.className = 'position-relative'
-          activeElement.parentNode.insertBefore(wrapper, activeElement)
-          wrapper.appendChild(activeElement)
-          activeElement.insertAdjacentHTML('afterend', spinnerHtml)
+          // In the submit row (change form),
+          // the button will be wrapped and
+          // the spinner put in absolute position.
+          if (isInSubmitRow) {
+            const wrapper = document.createElement('span')
+            wrapper.className = 'position-relative'
+            activeElement.parentNode.insertBefore(wrapper, activeElement)
+            wrapper.appendChild(activeElement)
+            activeElement.insertAdjacentHTML('afterend', spinnerHtml)
 
-        // In other <button>
-        // the spinner will just replace the content.
-        } else if (isSubmitButton) {
-          activeElement.innerHTML = spinnerHtml
-        }
+          // In other <button>
+          // the spinner will just replace the content.
+          } else if (isSubmitButton) {
+            activeElement.innerHTML = spinnerHtml
+          }
+        }, 0)
       })
     })
   })
