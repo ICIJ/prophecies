9aabded26bbb50c3bf44d08ae0e4f206dca75926
Pierre Romera <hello@pirhoo.com>
2024-01-31 20:27:32 +0100

    test: disable logging during tests

--- prophecies/settings/test.py
+++ prophecies/settings/test.py
@@ -1,8 +1,12 @@
+import logging
+
 from .base import *  # pylint: disable=wildcard-import,unused-wildcard-import
 
 DEBUG = False
 TEMPLATE_DEBUG = False
 USE_X_FORWARDED_HOST = False
 
-INSTALLED_APPS.remove('debug_toolbar')
-MIDDLEWARE.remove('debug_toolbar.middleware.DebugToolbarMiddleware')
+INSTALLED_APPS.remove("debug_toolbar")
+MIDDLEWARE.remove("debug_toolbar.middleware.DebugToolbarMiddleware")
+
+logging.disable(logging.CRITICAL)
