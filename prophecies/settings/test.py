44bc889fde154f3b1304a6b37ca52ab60c3c5435
Pierre Romera <hello@pirhoo.com>
2024-01-31 20:37:12 +0100

    test: remove constance public keys

--- prophecies/settings/test.py
+++ prophecies/settings/test.py
@@ -6,7 +6,14 @@ DEBUG = False
 TEMPLATE_DEBUG = False
 USE_X_FORWARDED_HOST = False
 
+# Deinstall Django Debug Toolbar
 INSTALLED_APPS.remove("debug_toolbar")
 MIDDLEWARE.remove("debug_toolbar.middleware.DebugToolbarMiddleware")
+
+# Ensure nothing is cached during test
+CACHE_URL = "dummycache://"
+
+# Hide all constance keys by default
+CONSTANCE_PUBLIC_KEYS = []
 
 logging.disable(logging.CRITICAL)
