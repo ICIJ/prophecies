79db1a703f3d376bc4b5eb251f3fa825b9386fe4
Miguel <miguel.fiandor.gutierrez@gmail.com>
2025-05-05 19:22:46 +0200

    clean: remove commented code

--- prophecies/core/admin/task_record_media_admin.py
+++ prophecies/core/admin/task_record_media_admin.py
@@ -117,39 +117,6 @@ class TaskRecordMediaAdmin(ExportWithCsvStreamMixin, admin.ModelAdmin):
         # Or display the upload form
         return self.upload_form_view(request)
 
-    # def upload_form_view(self, request, extra_context=None):
-    #     task = request.GET.get("task")
-    #     extra_context = extra_context or {}
-    #     initial = {
-    #         "task": task,
-    #         "unique": True,
-    #         "media_types": TaskRecordMedia.MediaType.values,
-    #     }
-    #     form = extra_context.get("form", TaskRecordMediaUploadForm(initial=initial))
-    #     title = "Upload task record medias"
-    #     context = self.build_intermediate_form_context(
-    #         request=request, form=form, title=title
-    #     )
-    #     return render(request, "admin/upload_form.html", context)
-
-    # def upload_form_handler(self, request):
-    #     form = TaskRecordMediaUploadForm(request.POST, request.FILES)
-    #     if form.is_valid():
-    #         # The save method will take care of handling the zip and upload task record medias
-    #         created, updated, ignored = form.save()
-    #         self.message_user(
-    #             request,
-    #             f"Your zip file has been imported: {created} created, {updated} updated and {ignored} ignored.",
-    #             messages.INFO,
-    #         )
-    #         # Everything is fine, go back to the list of task record medias
-    #         return redirect("..")
-    #     self.message_user(
-    #         request, "Unable to import the task record medias", messages.ERROR
-    #     )
-    #     # Call the same view with the received form
-    #     return self.upload_form_view(request, extra_context={"form": form})
-
     def upload_form_view(self, request, extra_context=None):
         task = request.GET.get('task')
         extra_context = extra_context or {}
